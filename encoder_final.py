d65ef98a94a4fa981a998fa833a60ac1c4b53e05
ogu1 <126238527+ogu1@users.noreply.github.com>
2023-02-24 18:50:31 +0900

    Add files via upload

---
+++ encoder_final.py
@@ -0,0 +1,93 @@
+# encoder final.ver
+
+import RPi.GPIO as GPIO
+import math
+import time
+
+LXPin = 7
+LAPin = 12
+LBPin = 1
+RXPin = 4
+RAPin = 27
+RBPin = 22
+
+#初期設定
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LXPin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.setup(LAPin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.setup(LBPin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.setup(RXPin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.setup(RAPin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.setup(RBPin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+#エンコーダのパルスをカウント
+def pulse_count(APin, BPin):
+    count = 0
+    start = time.perf_counter()
+    
+    while (time.perf_counter()-start < 0.25):
+        a = GPIO.wait_for_edge(APin, GPIO.FALLING, timeout=250)
+      
+        if a is None:
+            count = 0
+        else:
+            if GPIO.input(BPin) == 1:
+                count = count-1
+            else:
+                count = count+1
+    return(count)
+
+#タイヤがそれぞれ進んだ距離の算出(今は1秒ごと)
+def distance():
+    pulse = 500 #ppr
+    l = 0.4 #[m] タイヤが一回転する間に進む距離
+    k = 0.9 #値を調整するための係数
+    
+    R_count_1 = pulse_count(RAPin, RBPin)
+    L_count_1 = pulse_count(LAPin, LBPin)
+    L_count_2 = pulse_count(LAPin, LBPin)
+    R_count_2 = pulse_count(RAPin, RBPin)
+        
+    R_count = -(R_count_1+R_count_2)*2
+    L_count = (L_count_1+L_count_2)*2
+
+    R_d = l*R_count/pulse/k #[m]
+    L_d = l*L_count/pulse/k #[m]
+
+    return R_d, L_d
+
+#位置の推定
+def location(x, y, fai, R_d, L_d):
+    W = 0.15 #[m] タイヤ間距離
+
+    fai = fai+(R_d-L_d)/W #[rad]
+    x = x+(R_d+L_d)/2*math.cos(fai/2) #[m]
+    y = y+(R_d+L_d)/2*math.sin(fai/2) #[m]
+    
+    return x, y, fai
+
+#終了する用
+def destroy():
+    GPIO.cleanup()
+    print("program end")
+
+
+def loop():
+    x = 0
+    y = 0
+    fai = 0
+    
+    while True: #average
+        R_d, L_d = distance()
+        
+        x, y, fai = location(x, y, fai, R_d, L_d)
+        print(x, y)
+        #if x > 10:
+            #break
+    
+    #destroy()
+
+if __name__ == '__main__':
+    setup()
+    loop()
---
+++ motor_final.py
@@ -0,0 +1,127 @@
+#モーター
+
+#ライブラリのインポート
+import RPi.GPIO as GPIO
+import time
+import math
+
+#encoder_finalのインポート→よく分からんから任せた！
+from encoder_final import distance
+
+AIN1 = 16
+AIN2 = 18 
+PWMA = 12
+BIN1 = 36
+BIN2 = 38
+PWMB = 35
+
+#ここから初期設定#
+
+GPIO.setmode(GPIO.BOARD)
+
+#各ピンを出力に設定
+GPIO.setup(AIN1, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(AIN2, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(PWMA, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(BIN1, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(BIN2, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(PWMB, GPIO.OUT, initial = GPIO.LOW)
+
+#PWMオブジェクトのインスタンスを作成
+#出力ピン：12,35  周波数：100Hz
+p_a = GPIO.PWM(PWMA,100)
+p_b = GPIO.PWM(PWMB,100)
+
+#PWM信号を出力
+p_a.start(0)
+p_b.start(0)
+
+#デューティを設定(0~100の範囲で指定)
+#速度は80%で走行する。
+val = 80
+
+#デューティ比を設定
+p_a.ChangeDutyCycle(val)
+p_b.ChangeDutyCycle(val)
+#ここまで#
+
+#ブレーキの関数
+def brake():
+    GPIO.output(AIN1, GPIO.HIGH)
+    GPIO.output(AIN2, GPIO.HIGH)
+    GPIO.output(BIN1, GPIO.HIGH)
+    GPIO.output(BIN2, GPIO.HIGH)
+
+    time.sleep(3)  #3秒間は停止
+
+#前進の関数
+#d[m]進むまで前進
+def forward(d):
+    l = 0 #[m]これが進んだ距離
+
+    #モーターを正方向に回転
+    GPIO.output(AIN1, GPIO.LOW)
+    GPIO.output(AIN2, GPIO.HIGH)
+    GPIO.output(BIN1, GPIO.LOW)
+    GPIO.output(BIN2, GPIO.HIGH)
+
+    while(l < d):
+        #エンコーダで距離を計測(約1秒ごとに値を取得)
+        R_d, L_d = distance() #エンコーダのファイルより
+        l = l+(R_d+L_d)/2
+
+#後退の関数
+#d[m]戻るまで後退
+def back(d):
+    l = 0 #これが後退した距離
+
+    #モーターを逆方向に回転
+    GPIO.output(AIN1, GPIO.HIGH)
+    GPIO.output(AIN2, GPIO.LOW)
+    GPIO.output(BIN1, GPIO.HIGH)
+    GPIO.output(BIN2, GPIO.LOW)
+
+    while(l<d):
+        #エンコーダで距離を計測
+        R_d, L_d = -distance() #エンコーダのファイルより
+        l = l+(R_d+L_d)/2
+
+#回転の関数
+#進行方向に対して左にi[deg]だけ回転(-180<i<180)
+def rotation(i):
+    fai = 0 #[deg] ここに回転角を入力
+    W = 0.15 #[m] タイヤ間距離
+
+    #右輪は前進、左輪は後退(中心位置は固定)
+    GPIO.output(AIN1, GPIO.HIGH)
+    GPIO.output(AIN2, GPIO.LOW)
+    GPIO.output(BIN1, GPIO.LOW)
+    GPIO.output(BIN2, GPIO.HIGH)
+
+    while(abs(fai) < abs(i)):
+        R_d, L_d = distance() #エンコーダのファイルより
+        fai = fai+(R_d-L_d)/W*180/math.pi
+
+#終了用
+def destroy():
+    #PWM信号の停止
+    p_a.stop()
+    p_b.stop()
+
+    #GPIOピンの解放
+    GPIO.cleanup()
+
+    print("end of program")
+
+#試験用のプログラム
+def test(forward, back, turn):
+    forward(forward)
+    brake
+
+    back(back)
+    brake
+
+    rotation(turn)
+    brake
+
+    destroy()
