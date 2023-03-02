108ae85fc837f313bb8e2cfaf0fb5cbeadbead23
IkuY@ <51374858+XaiueoX@users.noreply.github.com>
2023-03-03 08:33:42 +0900

    Create serial.py

---
+++ serial.py
@@ -0,0 +1,50 @@
+import serial
+import time
+
+def serial_open():
+    try:
+        ser=serial.Serial('/dev/ttyAMA0', 115200, timeout=0.1)
+        print("open serial port")
+        serial_write("open serial port")
+    except:
+        print("cannot open serial port")
+        serial_write("cannot open serial port")
+        exit(1)
+
+def serial_write(msg):
+    ser.write((msg+"\r\n").encode("utf-8"))
+
+# def terminal_command(l):
+#     lsp = l.split(";")
+#     msg = lsp[7].split()
+#     if len(msg) > 0:
+#         if msg[0] == 'q':
+#             print("...quitting")
+#             serial_write("...quitting")
+#             time.sleep(0.5)
+#             exit(0)
+#         elif msg[0] == 'test':
+#             x = msg[1]
+#             y = msg[2]
+#             turn = msg[3]
+#             print(x,y,turn)
+#         else:
+#             print(msg)
+
+# def main():
+#     try:
+#         line = ser.readline().rstrip().decode("utf-8")
+#         terminal_command(line)
+
+#     except KeyboardInterrupt:
+#         print("...quitting")
+#         serial_write("...quitting")
+#         time.sleep(0.5)
+#         exit(0)
+#     except SystemExit:
+#         print("SystemExit")
+#         serial_write("SystemExit")
+#         exit(0)
+#     except:
+#         print("... unknown exception detected")
+#         exit(1)
